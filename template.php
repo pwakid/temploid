4597a07ed6450e82f5cb7c8f64740c0edb416695
pwakid <163606441+pwakid@users.noreply.github.com>
2024-03-18 04:13:29 -0400

    Update template.php

--- template.php
+++ template.php
@@ -4,7 +4,7 @@ class Template {
     protected $variables = [];
     protected $loops = [];
     protected $templatePath;
-    protected $templateContent;
+    protected $templateContent = '';
 
     public function __construct($config) {
         $this->templatePath = rtrim($config['root'], '/') . '/';
@@ -16,37 +16,49 @@ class Template {
         }
     }
 
-    public function loop($name, $data) {
+    public function loop($name, array $data) {
         if (!isset($this->loops[$name])) {
             $this->loops[$name] = [];
         }
         $this->loops[$name][] = $data;
     }
 
-    public function display($pageTitle, $templateName, $cssPath = null, $jsPath = null) {
-        $this->templateContent = file_get_contents($this->templatePath . $templateName . '.html');
+    public function display($pageTitle, $templateName) {
+        $templateFilePath = $this->templatePath . $templateName . '.html';
+
+        if (!file_exists($templateFilePath) || !is_readable($templateFilePath)) {
+            echo "Error: Template file not found or not readable.";
+            return;
+        }
+
+        $this->templateContent = file_get_contents($templateFilePath);
 
         // Replace static variables
         foreach ($this->variables as $key => $value) {
-            $this->templateContent = str_replace("{{$key}}", $value, $this->templateContent);
+            $this->templateContent = str_replace("{{$key}}", htmlspecialchars($value), $this->templateContent);
         }
 
         // Process loops
         foreach ($this->loops as $loopName => $loopData) {
-            if (empty($loopData)) {
-                $this->templateContent = preg_replace("/<!-- LOOP $loopName -->.*?<!-- LOOPELSE $loopName -->(.*?)<!-- END $loopName -->/s", '$1', $this->templateContent);
-            } else {
-                $loopContent = '';
-                $pattern = "/<!-- LOOP $loopName -->(.*?)<!-- END $loopName -->/s";
-                preg_match($pattern, $this->templateContent, $matches);
+            $loopContent = '';
+            $pattern = "/<!-- LOOP $loopName -->(.*?)<!-- END $loopName -->/s";
+
+            if (preg_match($pattern, $this->templateContent, $matches) && isset($matches[1])) {
+                $loopTemplate = $matches[1];
+
                 foreach ($loopData as $item) {
-                    $itemContent = $matches[1];
-                    foreach ($item as $key => $value) { 
-                        $loopNameKey = $loopName.".".$key;
-                        $itemContent = str_replace("{{$loopNameKey}}", $value, $itemContent);
+                    $itemContent = $loopTemplate;
+                    foreach ($item as $key => $value) {
+                      $loopNameKey = $loopName.".".$key;
+                        $itemContent = str_replace("{{$loopNameKey}}", htmlspecialchars($value), $itemContent);
                     }
                     $loopContent .= $itemContent;
                 }
+            }
+
+            if (empty($loopData)) {
+                $this->templateContent = preg_replace("/<!-- LOOP $loopName -->.*?<!-- LOOPELSE $loopName -->(.*?)<!-- END $loopName -->/s", '$1', $this->templateContent);
+            } else {
                 $this->templateContent = preg_replace($pattern, $loopContent, $this->templateContent);
             }
         }
